512a0197bd248af639128c4800f173f4f1349279
FenixGnom <mailgogo@mail.ru>
2012-05-16 21:53:23 +0400

    Написание функции решения квадратного уравнения

--- kvadrat.py
+++ kvadrat.py
@@ -3,13 +3,35 @@
 
 import math
 
-A = input(u'a: ')
-B = input(u'b: ')
-C = input(u'c: ')
+def solve(AA,BB,CC):
+    if type(AA) == float and type(BB) == float and type(CC) == float :
+        DD = BB*BB - 4.0*AA*CC
+        if DD >= 0.0:
+            x1 = (-BB + math.sqrt(DD)) / (2.0 * AA)
+            x2 = (-BB - math.sqrt(DD)) / (2.0 * AA)
 
-D = B*B - 4.0*A*C
+            return [x1, x2]
+        else:
+            return []
+    else :
+        return [u'Transferred to incorrect values']
 
-x1 = (-B + math.sqrt(D)) / (2.0 * A)
-x2 = (-B - math.sqrt(D)) / (2.0 * A)
 
-print x1, x2
+A = float(input(u'a: '))
+B = float(input(u'b: '))
+C = float(input(u'c: '))
+
+R = solve(A,B,C)
+print(R)
+
+#D = B*B - 4.0*A*C
+#if D >= 0.0 :
+#    x1 = (-B + math.sqrt(D)) / (2.0 * A)
+#    x2 = (-B - math.sqrt(D)) / (2.0 * A)
+#
+#    print x1, x2
+#
+#else :
+#    print u'No soution'
+#
+#print u'END'
