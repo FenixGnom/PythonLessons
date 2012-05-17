69c606e6b223cb47c0048bdbe807c079879a829a
FenixGnom <mailgogo@mail.ru>
2012-05-17 20:05:43 +0400

    аываыва

---
+++ kvadrat2.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+
+A = input(u'a: ')
+B = input(u'b: ')
+C = input(u'c: ')
+
+def solve():
+    try :
+        DD = B*B - 4.0*A*C
+        if DD >= 0.0:
+            x1 = (-B + math.sqrt(DD)) / (2.0 * A)
+            x2 = (-B - math.sqrt(DD)) / (2.0 * A)
+
+            return [x1, x2]
+        else:
+            return []
+    except TypeError:
+        print u'Transferred to incorrect values'
+
+R = solve()
+print(R)
